77ee3d11953b48df7066a3e16302c05d4095d31a
Admin <vladyhahavest@gmail.com>
2022-08-14 21:47:39 +0300

    part2_asyncio_decorator_failed

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
 import threading
 
+
 # функція для відрахунку
-def countdown_back(number):
+def countdown_back_thread(number):
     from time import sleep
 
     for i in range(number, 0, -1):
         print(i)
         sleep(1)
 
+
 # функція для запуску окремого потоку
 def countdown(number):
-    t = threading.Thread(target=countdown_back, args=(number,))
+    t = threading.Thread(target=countdown_back_thread, args=(number,))
     t.start()
 
 
-countdown(4)
-countdown(4)
-countdown(4)
+# countdown(4)
+# countdown(4)
+# countdown(4)
+
+import asyncio
+
+
+
+
+async def countdown_back_asyncio(number):
+
+    for i in range(number, 0, -1):
+        print(i)
+        await asyncio.sleep(1)
+
+def decor(func):
+    def wrapper(*args, **kwargs):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(func(*args, **kwargs))
+        return
+    return wrapper
+
+@decor
+async def countdown_asyncio(number):
+    t = asyncio.create_task(countdown_back_asyncio(number))
+    await t
+
+# loop = asyncio.get_event_loop()
+# tasks = asyncio.gather(countdown_asyncio(5),countdown_asyncio(5),countdown_asyncio(5) )
+# loop.run_until_complete(tasks)
+
+# через декоратор вони виконуються послідовно
+
+countdown_asyncio(5)
+countdown_asyncio(5)
+countdown_asyncio(5)
