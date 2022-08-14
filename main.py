14550dde83b53261f299c0c2dc105b71920fbbc0
Admin <vladyhahavest@gmail.com>
2022-08-14 21:06:48 +0300

    part1_threading

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import threading
 
+# функція для відрахунку
 def countdown_back(number):
     from time import sleep
 
@@ -7,14 +8,12 @@ def countdown_back(number):
         print(i)
         sleep(1)
 
-
+# функція для запуску окремого потоку
 def countdown(number):
     t = threading.Thread(target=countdown_back, args=(number,))
     t.start()
+
+
 countdown(4)
 countdown(4)
 countdown(4)
-
-
-
-
