166d63a6b51f67c542538943395a6e54f7f561a4
Admin <vladyhahavest@gmail.com>
2022-08-14 23:30:11 +0300

    test_of_productivity (asyncio is better)

--- main.py
+++ main.py
@@ -1,41 +1,56 @@
 import threading
+import asyncio
+from datetime import datetime
 
 
-# функція для відрахунку
 def countdown_back_thread(number):
     from time import sleep
 
     for i in range(number, 0, -1):
         print(i)
         sleep(1)
+        for k in range(10000000 * 3):
+            pass
 
 
-# функція для запуску окремого потоку
-def countdown(number):
-    t = threading.Thread(target=countdown_back_thread, args=(number,))
-    t.start()
+# def countdown(number):
+#     t = threading.Thread(target=countdown_back_thread, args=(number,))
+#     t.start()
 
 
-# countdown(4)
-# countdown(4)
-# countdown(4)
+start = datetime.now()
+t1 = threading.Thread(target=countdown_back_thread, args=(5,))
+t2 = threading.Thread(target=countdown_back_thread, args=(5,))
+t3 = threading.Thread(target=countdown_back_thread, args=(5,))
+t1.start()
+t2.start()
+t3.start()
 
-import asyncio
-
-
+t1.join()
+t2.join()
+t3.join()
+print(datetime.now() - start)
 
 
 async def countdown_back_asyncio(number):
-
     for i in range(number, 0, -1):
         print(i)
         await asyncio.sleep(1)
+        for k in range(10000000 * 3):
+            pass
 
 
 async def countdown_asyncio(number):
     t = asyncio.create_task(countdown_back_asyncio(number))
     await t
 
+
+start = datetime.now()
 loop = asyncio.get_event_loop()
-tasks = asyncio.gather(countdown_asyncio(5),countdown_asyncio(5),countdown_asyncio(5) )
+tasks = asyncio.gather(countdown_asyncio(5), countdown_asyncio(5), countdown_asyncio(5))
 loop.run_until_complete(tasks)
+print(datetime.now() - start)
+
+'''
+asyncio помітно більш продуктивний у даному випадку
+'''
